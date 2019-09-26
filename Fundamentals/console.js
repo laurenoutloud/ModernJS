27ce3836607254f24028c2f931f6b0549a3df7c7
Lauren <lauren.finley1@yahoo.com>
2019-09-26 22:42:56 +0900

    practice using console

---
+++ Fundamentals/console.js
@@ -0,0 +1,21 @@
+/*Sample Alert */
+//alert("Hello World from app.js");
+
+//log to console from application
+console.log("Hello World");
+console.log(123);
+console.log(1+2+3);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('Random error message');
+console.clear();
+console.warn('Random warning');
+console.time('Hello'); //Use 
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+console.timeEnd('Hello');
